a4a197a045bbdf093ec5968cbda51b20eb0d2399
divyanshu3725 <divyanshu3725@gmail.com>
2024-02-20 23:43:07 +0530

    day

---
+++ recursion/notes.cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+
+
+// EXAMPLE 3 FIBONACCI SERIES
+// int fibonacci(int n)
+// {  
+//     if(n==1 || n==2)
+//     {
+//         return 1;
+//     }
+//     else
+//     {
+//         return fibonacci(n-1) + fibonacci(n-2);
+//     };
+// };
+// int main()
+// {
+//     int n;
+//     cout<<"Enter the value of n: ";
+//     cin>>n;
+//     for(int i = 1; i<=n; i++)
+//     {
+//         cout<<fibonacci(i)<<endl;
+//     };
+// };
+
+// FROM CODING NINJAS
+// a recursive method solves a problem by calling a copy of itself to work on a smaller problem
+// Recursion uses an in-built stack that stores recursive calls. Hence, the number of recursive calls must be as small as possible to avoid memory-overflow. If the number of recursion calls exceeded the maximum permissible amount, the recursion depth* will be exceeded. This condition is called stack overflow.
+// 
+
+// EXTRA HOW TO WRITE SOME CODE THAT GET'S EXECUTED AFTER THE RECURSIVE CALL
+// int printer(int n)
+// {
+//     if(n<=0)
+//     {
+//         return -1;
+//     }
+//     else
+//     {
+//         // cout<<n<<endl; // this will give 5 4 3 2 1
+//         int result = printer(n-1);
+//         cout<<n<<endl; // this will give 1 2 3 4 5
+//         return result;
+//     }
+// };
+// int main()
+// {
+//     int n;
+//     cout<<"Enter the value of n: ";
+//     cin>>n;
+//     printer(n);
+// }
+
+// EXAMPLE 3 PRINTER FUNCTION (FROM 1 TO N)
+// int printer(int n)
+// {
+//     if(n<=0)
+//     {
+//         return -1;
+//     };
+//     // cout<<n<<endl; // this will give 5 4 3 2 1 
+//     printer(n-1);
+//     cout<<n<<endl; // this will give 1 2 3 4 5
+// };
+// int main()
+// {
+//     int n;
+//     cout<<"Enter the value of n: ";
+//     cin>>n;
+//     printer(n);
+// };
+
+// EXAMPLE 2 POWER FUNCTION
+// int power(int a, int b)
+// {
+//     if (a == 0)
+//     {
+//         return 0;
+//     }
+//     else if (a == 1)
+//     {
+//         return 1;
+//     }
+//     else
+//     {
+//         if (b < 0)
+//         {
+//             cout << "Please enter a non negative power: ";
+//             return -1;
+//         }
+//         else if (b == 0)
+//         {
+//             return 1;
+//         }
+//         else
+//         {
+//             return a * power(a, b - 1);
+//         };
+//     };
+// };
+// int main()
+// {
+//     int a;
+//     cout << "Enter the value of a: ";
+//     cin >> a;
+//     int b;
+//     cout << "Enter the value of b: ";
+//     cin >> b;
+//     cout << power(a, b) << endl;
+// };
+
+// HEAD RECURSION
+// RECURSION WITH THE FOLLOWING ORDER:
+// BASE CASE
+// RECURSIVE RELATION
+// PROCESSING
+
+// TAIL RECURSION
+// RECURSION WITH THE FOLLOWING ORDER:
+// BASE CASE
+// PROCESSING
+// RECURSIVE RELATION
+
+// RECURSION FUNCTION PARTS:
+// BASE CASE
+// PROCESSING
+// RECURSIVE RELATION
+
+// RECURSION TREE
+// [4] (returns 24)
+//    [3] (returns 6)
+//       [2] (returns 2)
+//          [1] (returns 1)
+//             [0] (returns 1)
+
+// EXAMPLE 1 FACTORIAL FUNCTION
+// int factorial(int n)
+// {
+//     if(n<0)
+//     {
+//         cout<<"please enter a positive number!"<<endl;
+//         return -1;
+//     }
+//     else if(n==0 || n==1)
+//     {
+//         return 1;
+//     }
+//     else
+//     {
+//         return n*factorial(n-1);
+//     };
+// };
+// int main()
+// {
+//     int n;
+//     cout<<"Enter the value of n: ";
+//     cin>>n;
+//     cout<<factorial(n)<<endl;
+//     // IF WE DON'T SUPPLY THE BASE CASE THEN WE WILL GET A SEGMENTATION FAULT
+//     // NOT SUPPLYING A BASE CASE WILL CAUSE STACK OVERFLOW
+//     // BECAUSE THE STACK OVERFLOWS
+//     // THE MAIN FUNCTION ENTERED THE STACK FIRST
+//     // THEN NUMEROUS FACTORIAL STARTED ENTERING THE STACK
+//     // FACTORIAL FUNCTIONS ARE ENTERING THE STACK ONE BY ONE
+//     // STACK OVERFLOW!
+// }
+
+// INTORDUCTION TO RECURSION
+// int main()
+// {
+//     // RECURSION IN PROGRAMMING REFERS TO THE TECHNIQUE IN WHICH A FUNCTION CALLS ITSELF IN ORDER TO SOLVE A PROBLEM
+//     // 2^4 = 2 * 2^3 (THIS IS A RECURSIVE RELATION)
+//     // EXAMPLE: FACTORIAL CALCULATOR FUNCTION
+//     // WE NEED A BASE CASE SO THAT WE DON'T RUN INTO AN INFINITE LOOP
+//     //
+// };
